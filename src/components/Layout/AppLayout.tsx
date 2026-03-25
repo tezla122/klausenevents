db6f5867fa86562a60e847599bad2e6095394231
DESKTOP-HP6H48I\dan <rotardan27@gmail.com>
2026-03-25 23:54:00 +0200

    Deleted MobileNavbar

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -9,8 +9,7 @@ export function AppLayout({ children }: AppLayoutProps): React.JSX.Element {
   return (
     <div className="flex min-h-screen min-w-0 flex-col overflow-x-hidden">
       <DesktopNavbar />
-      {/* Offset for fixed mobile header; md+ uses sticky navbar in document flow */}
-      <main className="flex min-h-0 min-w-0 flex-1 flex-col pt-14 md:pt-0">
+      <main className="flex min-h-0 min-w-0 flex-1 flex-col">
         {children}
       </main>
     </div>
